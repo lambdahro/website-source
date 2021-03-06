3f76d441b446393a2590be9516e3b875028be58c
Jens Grassel <jens@wegtam.com>
2021-03-06 18:30:43 +0100

    Fix branch

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ read -p "Commit and publish changes? [yn] " yn
 case "${yn}" in
   [Yy]* )
     echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-    cd public && git add . && git commit -m "Publish website." && git push origin HEAD:master
+    cd public && git add . && git commit -m "Publish website." && git push origin HEAD:main
     cd "${CURDIR}"
     exit 0
     ;;
